935e6066d8ae2d2812cf33c3996fe931514bcb78
Marcel Raichle <r.marcello2008@gmail.com>
2025-04-02 22:26:18 +0200

    Viel spaß

--- Game/src/main/java/org/example/Main.java
+++ Game/src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+/*
+TODO:
+überleg dir halt noch Characktere so das wir auf 10 Stück kommen
+für Javafx schau dir sowas an das du bissl mehr checkst (das is jtz nur n vorschlag kannst schauen ob du was besseres findest
+https://zetcode.com/gui/javafx/firstprograms/
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+ */
+        System.out.println("Hallo Sophie!");
+
+/*
+
+1. kuhle Projektstruktur die hoffentlich so passt
+Main.java: Startet das Spiel Auswahlmenü für den Spielmodus (es gibt jtz mal nur einen vllt wenn mir langweilig ist kommt der 2te)
+GameFrame.java: erstellt das Hauptspiel-Fenster
+GamePanel.java: für die Spiellogik und das Rendering.
+Charakterklassen (Swordsman.java, Knight.java, Mage.Java, Ninja.java): Spielcharaktere mit ihren Angriffen und Eigenschaften.
+
+2. Steuerung und so
+Spieler 1: Steuert mit WASD und Q,E für attacken
+Spieler 2: Steurt mit Pfeiltasten ka miz was der angreifen wird such dir was aus
+jeder hat 2 attacken
+ */
     }
 }
